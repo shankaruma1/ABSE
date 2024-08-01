6d7bf7a57f19061991199c82d6b5547d4646e996
shankaruma1 <umasankararao.v@srmap.edu.in>
2024-08-01 15:56:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class TLABKS(ABEnc):
         self.util = MSP(self.group, verbose)
 
     def setup(self, U):
+        start_time = datetime.now()
         """
         This algorithm generates public parameters and master secret key.
         U is an universal attribute set
@@ -35,9 +36,12 @@ class TLABKS(ABEnc):
           g_pwr_a_i = g1 ** a_i
         pp = {'g1': g1, 'p_pow_beta':p_pow_beta, ' g_pwr_alpha': g_pwr_alpha, 'g_pwr_a_i':g_pwr_a_i, 'e_pair':e_pair}
         msk = {'alpha':alpha, 'beta': beta}
+        end_time = datetime.now()
+        print('Duration: {}'.format(end_time - start_time))
         return pp, msk
 
     def keygen(self, pp, msk, uid, attr_list):
+        start_time = datetime.now()
         """
         Generate a sectrt key for given set of attributes.
         """
@@ -61,16 +65,19 @@ class TLABKS(ABEnc):
             Di1 = g1 ** n2
             Di2 = pp['g_pwr_a_i'] ** n2  
             D[attr] = (Di1, Di2)
+        end_time = datetime.now()
+        print('Duration: {}'.format(end_time - start_time))
 
         return {'attr_list': attr_list, 'D1': D1, 'D2': D2, 'D3': D3, 'D4': D4, 'D': D}
 
     def do_enc(self, pp, F, policy_str):
+        start_time = datetime.now()
         """
          Encrypt a file F under a policy string. This is an intermediate encryption stage
         """
 
         if debug:
-            print('Encryption algorithm:\n')
+            print('Data owner encryption algorithm:\n')
 
         policy = self.util.createPolicy(policy_str)
         mono_span_prog = self.util.convert_policy_to_msp(policy)
@@ -95,24 +102,36 @@ class TLABKS(ABEnc):
                 Xi += row[i] * u[i]
           ICT = {}
           ICT = (c0,c1,c2,)
+        end_time = datetime.now()
+        print('Duration: {}'.format(end_time - start_time))
          
-          return {'policy': policy, 'ICT': ICT, 'Xi': Xi}
+        return {'policy': policy, 'ICT': ICT, 'Xi': Xi}
     
-    def edge-enc(self, PP, ICT, Xi):
+    def edge_enc(self, PP, ICT, Xi):
+        start_time = datetime.now()
+       """
+         This algorithm performs full encryption at edge node
         """
-        performs full encryption at edge device
-        """
+
+        if debug:
+            print('Edge encryption algorithm:\n')
           for attr, row in mono_span_prog.items():
             cc = self.group.random(ZR)
             c_i1 = g1 ** Xi
             c_i2 = pp['g_pwr_a_i'] ** cc
             c_i3 = g1 ** cc
+        end_time = datetime.now()
+        print('Duration: {}'.format(end_time - start_time))
           
         return {'ICT': ICT, 'c_i1':c_i1, 'c_i2':c_i2, 'c_i3':c_i3}
+        
     def IndexGen(self, PP, KW):
+        start_time = datetime.now()
       """
       This algorithm generates encrypted index
       """
+      if debug:
+          print('Index generation algorithm:\n')
       t1 = self.group.random(ZR)
       t2 = self.group.random(ZR)
       I2 = g1 ** t1
@@ -120,10 +139,19 @@ class TLABKS(ABEnc):
       I4 = pp['beta'] ** t2
       I1 = {}
       for kw in KW:
-        hs = self.group.hash(kw, G1)
-        I1 = pp['beta'] ** (hs * t1)
-      return {'I1':I1, 'I2':I2, 'I3':I3, 'I4':I4}
+            hs = self.group.hash(kw, G1)
+            I1 = pp['beta'] ** (hs * t1)
+       end_time = datetime.now()
+       print('Duration: {}'.format(end_time - start_time))
+       return {'I1':I1, 'I2':I2, 'I3':I3, 'I4':I4}
+        
     def trapgen(self, KWprime, D1, D3, D4):
+        start_time = datetime.now()
+       """
+      This algorithm generates trapdoor
+      """
+      if debug:
+          print('Trapdoor generation algorithm:\n')
       u = self.group.random(ZR)
       T2 = g1 ** u
       T3 = D3 ** u
@@ -132,9 +160,12 @@ class TLABKS(ABEnc):
       for kw in KWprime:
          hs = self.group.hash(kw, G1)
          T1 =pp['beta'] ** (hs * u)
+      end_time = datetime.now()
+      print('Duration: {}'.format(end_time - start_time))
       return {'T1':T1, 'T2':T2, 'T3':T3, 'T4':T4}
 
     def search(self, I1, I2, I3, I4, T1, T2, T3, T4):
+        start_time = datetime.now()
         """
          This algorithm searches between index and trapdoor
         """
@@ -147,8 +178,12 @@ class TLABKS(ABEnc):
         s1 = pair(T2, I1)
         if (s1 * s2) == (s3 * s4):
             return True
+        end_time = datetime.now()
+        print('Duration: {}'.format(end_time - start_time))
+        return
 
     def edge_dec(self, D1, D2, Di1, Di2, C1, C2, Ci2, Ci3):
+        start_time = datetime.now()
         """
          This algorithm performs partial decryption at edge node
         """
@@ -168,9 +203,12 @@ class TLABKS(ABEnc):
             (c_attr1, c_attr2) = ctxt['C'][attr]
             (k_attr1, k_attr2) = key['K'][attr_stripped]
             ICT = pair(D2, ed1) * pair(k_attr1, c_attr1) / pair(c_attr2, k_attr2)
+        end_time = datetime.now()
+        print('Duration: {}'.format(end_time - start_time))
         return {'ICT':ICT}
 
     def du_dec(self, ICT, D1, D3, D4):
+        start_time = datetime.now()
         """
          This algorithm performs full decryption
         """
@@ -191,8 +229,12 @@ class TLABKS(ABEnc):
             wi = self.group.random(ZR)
             E = pair(fd1, Ci1) ** wi
         F = (C0 * E)/T
+        end_time = datetime.now()
+        print('Duration: {}'.format(end_time - start_time))
         return {'F':F}
+        
     def keysanitycheck(self, D1, D2, D3, D4, Di1, Di2):
+        start_time = datetime.now()
          """
          This algorithm checks whether the given secret key is valid or not
         """
@@ -208,7 +250,11 @@ class TLABKS(ABEnc):
         kse4 = pair(pp['g_pow_ai', Di1)
         if (kse1 = = kse2) && (kse3 = = kse4):
             return True
+        end_time = datetime.now()
+        print('Duration: {}'.format(end_time - start_time))
+        return 
     def trace(self, D1):
+        start_time = datetime.now()
          """
          This algorithm returns user id
          """
@@ -216,5 +262,7 @@ class TLABKS(ABEnc):
         if debug:
             print('Trace algorithm:\n')
         uid = f.decrept(x)
+        end_time = datetime.now()
+        print('Duration: {}'.format(end_time - start_time))
         return {'uid':uid}
         
